81d5e51b45752a7f9cb90f1aa9df2ac650cf4ffa
Joe Constable <joeconstable97@gmail.com>
2026-02-01 17:04:47 +0000

    Fix typo in Steam plugin authentication message

--- Assets/Scripts/NXDSteamPlugin/SteamPlugin.cs
+++ Assets/Scripts/NXDSteamPlugin/SteamPlugin.cs
@@ -123,7 +123,7 @@ namespace NXDSteamPlugin
             text.font = Resources.Load<Font>("NXD");
             text.fontSize = 26;
             text.supportRichText = true;
-            text.text = $"WARNING: Steam will display this login as the following:\n<b>\"Mobile Device - NXD-{SystemInfo.deviceName}\".</b>\n\nPlease can the QR below to authenticate:";
+            text.text = $"WARNING: Steam will display this login as the following:\n<b>\"Mobile Device - NXD-{SystemInfo.deviceName}\".</b>\n\nPlease scan the QR below to authenticate:";
 
             var qrCodeObj = new GameObject("QRCode", typeof(RectTransform), typeof(RawImage), typeof(LayoutElement), typeof(AspectRatioFitter));
             qrCodeObj.transform.SetParent(root.transform, false);
